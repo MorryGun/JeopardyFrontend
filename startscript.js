9cfe48a8a8573b70d3406c13b7cd603ef20c4603
MorryGun <49810415+MorryGun@users.noreply.github.com>
2021-11-27 18:04:18 +0300

    Update startscript.js
    Update startscript.js

--- startscript.js
+++ startscript.js
@@ -1,1 +1,7 @@
-var cmd=require('node-cmd'); cmd.run('npm start');
+const { execSync } = require("child_process");
+
+// first two args can be ignored rest will be passed directly to the npm command
+const [ingore, ignore2, ...args] = process.argv;
+
+// windowsHide option will hide the cmd window
+execSync(`npm ${args.join(" ")}`, { windowsHide: true, stdio: "inherit" });
